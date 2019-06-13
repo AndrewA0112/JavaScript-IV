b96cd4a399c44d155430fcb8a44b7c296e526c46
Andrew <andrew.mkal@outlook.com>
2019-06-13 13:04:16 -0500

    Added Person, and Instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,21 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(attr) {
+        this.name = attr.name;
+        this.age = attr.age;
+        this.location = attr.location;
+    }
+
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
+    }
+}
+
+const test = new Person({
+    name: 'Fred',
+    age: 12,
+    location: 'asd'
+})
+
+test.speak();
